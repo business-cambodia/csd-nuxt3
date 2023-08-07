e107b86d81cd25c635d3310cc3808580222c4790
NarithNR9 <narith.neymar.jr@gmail.com>
2023-08-07 10:09:42 +0700

    [modify]

--- app.vue
+++ app.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+
+<script setup>
+import { initFlowbite } from 'flowbite';
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initFlowbite();
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.cdnfonts.com/css/proxima-nova-condensed',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Katibeh&display=swap',
+        },
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      ],
+    },
+  },
   css: ['~/assets/css/main.css'],
   postcss: {
     plugins: {
