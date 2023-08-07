11d101d7a6c4affa09ba6ef2f212a3ab130d2649
NarithNR9 <narith.neymar.jr@gmail.com>
2023-08-07 16:01:59 +0700

    [modify] fix responsive 4th

--- components/activity.vue
+++ components/activity.vue
@@ -1,14 +1,14 @@
 <template>
   <div
-    class="fixed maxw top-[230px] top-pos left-1/2 transform -translate-x-1/2 -translate-y-1/3 p-4 z-30 w-full px-6 py-0 font-proxima max-w-[350px]"
+    class="fixed maxw top-[230px] top-pos left-1/2 transform -translate-x-1/2 -translate-y-1/3 p-4 z-30 w-full px-6 py-0 font-proxima max-w-[340px]"
   >
-    <div class="absolute top-3 left-10 flex flex-col space-y-2">
+    <div class="absolute top-3 left-6 flex flex-col space-y-2">
       <div class="w-2 h-2 self-end rounded-full bg-primary"></div>
       <div class="w-12 h-12 rounded-full bg-primary"></div>
     </div>
     <!-- above-image -->
     <img src="/activities.png" class="z-40 w-full h-[550px] relative small-screen" alt="" />
-    <div class="absolute bottom-6 to right-12 flex flex-col space-y-2">
+    <div class="absolute bottom-3 to right-6 flex flex-col space-y-2">
       <div class="w-12 h-12 rounded-full bg-primary"></div>
       <div class="w-2 h-2 rounded-full bg-primary"></div>
     </div>
@@ -28,6 +28,7 @@
 @media (max-height:700px) {
   .small-screen {
     height: 450px;
+    width: 90%;
   }
   .top-pos {
     top: 200px;
