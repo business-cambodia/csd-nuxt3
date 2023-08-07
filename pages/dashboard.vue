e8ce37de00f65fc8386f0fdec12ff53c79e7e827
NarithNR9 <narith.neymar.jr@gmail.com>
2023-08-07 10:08:51 +0700

    [new] add dashboard

---
+++ pages/dashboard.vue
@@ -0,0 +1,98 @@
+<template>
+  <div class="mx-4 my-4 lg:mx-12">
+    <div class="text-4xl text-center bg-  -800 mb-6">
+      Voucher Dashboard
+    </div>
+    <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead
+          class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+        >
+          <tr>
+            <th scope="col" class="px-6 py-3">Name</th>
+            <th scope="col" class="px-6 py-3">Phone</th>
+            <th scope="col" class="px-6 py-3">Voucher Code</th>
+            <th scope="col" class="px-6 py-3">Voucher Type</th>
+            <th scope="col" class="px-6 py-3">Expired At</th>
+            <th scope="col" class="px-6 py-3">Status</th>
+            <th scope="col" class="px-6 py-3">
+              <span class="sr-only">Edit</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+          >
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              Apple MacBook Pro 17"
+            </th>
+            <td class="px-6 py-4">Silver</td>
+            <td class="px-6 py-4">Laptop</td>
+            <td class="px-6 py-4">$2999</td>
+            <td class="px-6 py-4">$2999</td>
+            <td class="px-6 py-4">$2999</td>
+            <td class="px-6 py-4 text-right">
+              <a
+                href="#"
+                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >Edit</a
+              >
+            </td>
+          </tr>
+          <tr
+            class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+          >
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              Microsoft Surface Pro
+            </th>
+            <td class="px-6 py-4">White</td>
+            <td class="px-6 py-4">Laptop PC</td>
+            <td class="px-6 py-4">$1999</td>
+            <td class="px-6 py-4">$1999</td>
+            <td class="px-6 py-4">$1999</td>
+            <td class="px-6 py-4 text-right">
+              <a
+                href="#"
+                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >Edit</a
+              >
+            </td>
+          </tr>
+          <tr
+            class="bg-white dark:bg-gray-800 hover:bg-gray-50 dark:hover:bg-gray-600"
+          >
+            <th
+              scope="row"
+              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+            >
+              Magic Mouse 2
+            </th>
+            <td class="px-6 py-4">Black</td>
+            <td class="px-6 py-4">Accessories</td>
+            <td class="px-6 py-4">$99</td>
+            <td class="px-6 py-4">$99</td>
+            <td class="px-6 py-4">$99</td>
+            <td class="px-6 py-4 text-right">
+              <a
+                href="#"
+                class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                >Edit</a
+              >
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
