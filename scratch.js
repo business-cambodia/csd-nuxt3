e9f5ae7c80d677292a48373be6b12588139eba87
NarithNR9 <narith.neymar.jr@gmail.com>
2023-08-09 13:04:02 +0700

    [modify] bigger scratch size and dynamic vouchers

--- scratch.js
+++ scratch.js
@@ -1,9 +1,15 @@
 if (!process.server) {
   var num = Math.floor(Math.random() * 2);
-  const vouchersImg = ['Vouchers-c.jpg', 'Vouchers-2-c.jpg'];
+  const vouchersImg = ['Vouchers.jpg', 'Vouchers-2.jpg'];
+  var voucher = useVoucher();
+  voucher.value = {
+    id: 'V-00' + (num + 1),
+    image: vouchersImg[num],
+    type: num == 0 ? '50$ OFF' : '1 Free Room',
+  };
 
   $('#card').wScratchPad({
-    size: 20, // The size of the brush/scratch.
+    size: 40, // The size of the brush/scratch.
     bg: vouchersImg[num], // Background (image path or hex color).
     fg: `/scratch.jpg`, // Foreground (image path or hex color).
     cursor: 'pointer', // Set cursor.
