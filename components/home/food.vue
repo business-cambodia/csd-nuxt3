b44a41329c4b4f0d84a658616a5573c5029f58e4
NarithNR9 <narith.neymar.jr@gmail.com>
2023-10-20 10:42:39 +0700

    [modify] add language to food

--- components/home/food.vue
+++ components/home/food.vue
@@ -2,20 +2,17 @@
   <div class="my-6 mx-4">
     <div class="flex justify-between">
       <NuxtLink
-        class="text-gradient-green-blue text-xl md:text-2xl font-bold mb-3"
+        class="text-gradient-green-blue text-xl md:text-2xl font-bold mb-3 leading-[2]"
         to="/"
       >
-        Foods
+        {{
+          (language === 'KH' && 'ម្ហូប') || (language === 'CN' && '') || 'Foods'
+        }}
       </NuxtLink>
 
       <div class="flex space-x-3 items-center">
         <div @click="foodSlide.prev()">
-          <IconsArrowLeft
-            :active="
-              foodSlide?.data?.currentSlide?.value !=
-              0
-            "
-          />
+          <IconsArrowLeft :active="foodSlide?.data?.currentSlide?.value != 0" />
         </div>
         <div @click="foodSlide.next()">
           <IconsArrowRight
@@ -32,8 +29,7 @@
         <CardsFood :food="a" />
       </Slide>
 
-      <template #addons>
-      </template>
+      <template #addons> </template>
     </Carousel>
   </div>
 </template>
@@ -44,11 +40,10 @@ import { IFood } from 'types/food';
 import { Carousel, Slide } from 'vue3-carousel';
 import 'vue3-carousel/dist/carousel.css';
 const foodSlide = ref();
+const language = useLanguague();
 
 const activities: IFood[] = (
-  await (<Promise<IResponse<IFood[]>>>(
-    useApi('items/foods', { method: 'GET' })
-  ))
+  await (<Promise<IResponse<IFood[]>>>useApi('items/foods', { method: 'GET' }))
 ).data;
 
 //responsive breakpoints for carousel
