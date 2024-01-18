e93f55e3b469c05a5293744a125cfdad086e3dd8
NarithNR9 <narith.neymar.jr@gmail.com>
2024-01-18 16:37:05 +0700

    [modify] change route middleware algorithm

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,11 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-  const user = useUser();
-  if (!user.value) {
-    return navigateTo('/auth');
+  if (process.client) {
+    if (!localStorage.getItem('user')) {
+      return navigateTo('/auth');
+    }
   }
-
+  // const user = useUser();
+  // if (!user.value) {
+  //   return navigateTo('/auth');
+  // }
 });
